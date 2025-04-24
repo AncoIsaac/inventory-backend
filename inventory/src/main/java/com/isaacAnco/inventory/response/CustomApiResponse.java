a30fe0586e09b526bc28d50c62432402e8186e3d
AncoIsaac <166188398+AncoIsaac@users.noreply.github.com>
2025-04-24 08:57:53 -0600

    add

--- inventory/src/main/java/com/isaacAnco/inventory/response/CustomApiResponse.java
+++ inventory/src/main/java/com/isaacAnco/inventory/response/CustomApiResponse.java
@@ -2,23 +2,15 @@ package com.isaacAnco.inventory.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Data
+@Getter
+@Setter
 public class CustomApiResponse {
     private String status;
     private Object data;
     private String message; // Nuevo campo para mensajes de error
-
-    // Constructor para respuestas exitosas
-    public CustomApiResponse(String status, Object data) {
-        this.status = status;
-        this.data = data;
-    }
-
-    // Constructor para errores
-    public CustomApiResponse(String status, String message) {
-        this.status = status;
-        this.message = message;
-    }
 }
